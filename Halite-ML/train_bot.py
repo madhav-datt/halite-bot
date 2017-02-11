4115e54f1f7c3ac2c35d2c7c4ec7a02210a2cdcc
Madhav Datt <madhav.datt@hotmail.com>
2017-02-11 14:14:57 +0530

    update training script

--- Halite-ML/train_bot.py
+++ Halite-ML/train_bot.py
@@ -16,7 +16,7 @@ training_target = []
 
 VISIBLE_DISTANCE = 4
 input_dim=4*(2*VISIBLE_DISTANCE+1)*(2*VISIBLE_DISTANCE+1)
-np.random.seed(0) # for reproducibility
+np.random.seed(0)  # for reproducibility
 
 model = Sequential([Dense(512, input_dim=input_dim),
                     LeakyReLU(),
@@ -48,38 +48,80 @@ for index, replay_name in enumerate(os.listdir(REPLAY_FOLDER)):
     strength = frames[:,:,:,1]
 
     moves = (np.arange(5) == np.array(replay['moves'])[:,:,:,None]).astype(int)[:128]
-    stacks = np.array([player==target_id,(player!=target_id) & (player!=0),prod/20,strength/255])
-    stacks = stacks.transpose(1,0,2,3)[:len(moves)].astype(np.float32)
 
-    position_indices = stacks[:,0].nonzero()
-    sampling_rate = 1/stacks[:,0].mean(axis=(1,2))[position_indices[0]]
-    sampling_rate *= moves[position_indices].dot(np.array([1,10,10,10,10])) # weight moves 10 times higher than still
-    sampling_rate /= sampling_rate.sum()
-    sample_indices = np.transpose(position_indices)[np.random.choice(np.arange(len(sampling_rate)),
-                                                                    min(len(sampling_rate),2048),p=sampling_rate,replace=False)]
+    # Rotate map 4 times and take mirror images to get 8 configurations for each map
+    moves_rotations = [moves]
 
-    replay_input = np.array([stack_to_input(stacks[i],[j,k]) for i,j,k in sample_indices])
-    replay_target = moves[tuple(sample_indices.T)]
-    
-    training_input.append(replay_input.astype(np.float32))
-    training_target.append(replay_target.astype(np.float32))
+    # # Rotated over all 3 configurations
+    # for _ in range(0, 3):
+    #     # Single standard rotation
+    #     moves_next = moves.copy()  # copy.deepcopy(moves)
+    #     for moves_next_l1 in moves_next:
+    #         for moves_next_l2 in moves_next_l1:
+    #             for moves_next_l3 in moves_next_l2:
+    #                 index = moves_next_l3.argmax()
+    #                 if index == 0:
+    #                     continue
+    #
+    #                 moves_next_l3[index] = 0
+    #                 next_index = index + 1
+    #                 if next_index == 5:
+    #                     next_index = 1
+    #                 moves_next_l3[next_index] = 1
+    #
+    #     moves_rotations.append(moves_next)
+    #     moves = moves_next
+    #
+    moves_mirrored = []
+    # # North south mirroring for all 4 configurations
+    # for moves in moves_rotations:
+    #     # Single standard rotation
+    #     moves_next = moves.copy()  # copy.deepcopy(moves)
+    #     for moves_next_l1 in moves_next:
+    #         for moves_next_l2 in moves_next_l1:
+    #             for moves_next_l3 in moves_next_l2:
+    #                 index = moves_next_l3.argmax()
+    #                 if index == 0:
+    #                     continue
+    #
+    #                 moves_next_l3[1], moves_next_l3[3] = moves_next_l3[3], moves_next_l3[1]
+    #
+    #     moves_mirrored.append(moves_next)
+
+    for moves in moves_rotations + moves_mirrored:
+        stacks = np.array([player==target_id,(player != target_id) & (player!=0),prod/20,strength/255])
+        stacks = stacks.transpose(1,0,2,3)[:len(moves)].astype(np.float32)
+
+        position_indices = stacks[:,0].nonzero()
+        sampling_rate = 1/stacks[:,0].mean(axis=(1, 2))[position_indices[0]]
+        sampling_rate *= moves[position_indices].dot(np.array([1,15,15,15,15])) # weight moves 10 times higher than still
+        sampling_rate /= sampling_rate.sum()
+        sample_indices = np.transpose(position_indices)[np.random.choice(np.arange(len(sampling_rate)),
+                                                                        min(len(sampling_rate),2048),p=sampling_rate,replace=False)]
+
+        replay_input = np.array([stack_to_input(stacks[i],[j,k]) for i,j,k in sample_indices])
+        replay_target = moves[tuple(sample_indices.T)]
+
+        training_input.append(replay_input.astype(np.float32))
+        training_target.append(replay_target.astype(np.float32))
 
 now = datetime.datetime.now()
 tensorboard = TensorBoard(log_dir='./logs/'+now.strftime('%Y.%m.%d %H.%M'))
 training_input = np.concatenate(training_input,axis=0)
 training_target = np.concatenate(training_target,axis=0)
 indices = np.arange(len(training_input))
-np.random.shuffle(indices) #shuffle training samples
+
+np.random.shuffle(indices)  # shuffle training samples
 training_input = training_input[indices]
 training_target = training_target[indices]
 
 model.fit(training_input,training_target,validation_split=0.2,
           callbacks=[EarlyStopping(patience=10),
-                     ModelCheckpoint('model.h5',verbose=1,save_best_only=True),
+                     ModelCheckpoint('model-3.h5',verbose=1,save_best_only=True),
                      tensorboard],
           batch_size=1024, nb_epoch=1000)
 
-model = load_model('model.h5')
+model = load_model('model-3.h5')
 
 still_mask = training_target[:,0].astype(bool)
 print('STILL accuracy:',model.evaluate(training_input[still_mask],training_target[still_mask],verbose=0)[1])
